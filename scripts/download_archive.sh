f90a636bdb3ed862fa558c748154c54d35cac283
PUTENCHIK <opaolifirenkomaxim@gmail.com>
2024-06-20 14:49:50 +0000

    Added comments in code and minor changes

--- scripts/download_archive.sh
+++ scripts/download_archive.sh
@@ -15,18 +15,23 @@ for filename in temporary/*.crx.gz; do
 	gunzip $filename;
 done
 
-# Creating directory files/ and special directory for date archive
+# Creating directory files/ if not exists
 if ! [ -d files ]; then
 	mkdir files;
 fi
 
 for filename in temporary/*.crx; do
+
+	# Separating name of station from crx file's name
 	station=$(echo $filename | sed 's|temporary/\([^_]*\)_.*|\1|')
 	if ! [ -d files/$station ]; then
+		# Creating directory for station if not exists
         	mkdir files/$station;
 	fi
 	new_name=${filename::-4}.'rnx';
+	echo "converting station's file: $filename";
 	sudo ./CRX2RNX $filename;
+	# Moving converted RINEX file from temporary directory to files/{dtation name}/{date of file} 
 	mv $new_name files/$station/$1.rnx;
 done
 
--- scripts/services_generator.sh
+++ scripts/services_generator.sh
@@ -1,31 +1,37 @@
 #!/bin/bash
 
+# Reading content of template of daemons
 template=$(<services/service_template)
 
 user="$(whoami)";
 path="$(pwd)";
 
 for dir in files/*/; do
-    receiver=$(basename "$dir")
-
-    if [[ -f "/etc/systemd/system/$receiver.service" ]]; then
-        continue
-    fi
-
-    rnx_file=$(find "$dir" -name '*.rnx' | sort)
-    rnx_file=${rnx_file[0]}
+	# Getting name of station
+	station=$(basename "$dir")
+	
+	# If necessary daemon already exists, iteration will be skip
+	if [[ -f "/etc/systemd/system/$station.service" ]]; then
+		continue
+	fi
+	
+	# Taking first RINEX file from station directory
+	rnx_file=$(find "$dir" -name '*.rnx' | sort)
+	rnx_file=${rnx_file[0]}
     
-    if [[ -n $rnx_file ]]; then
-        date=$(basename "$rnx_file")
+	if [[ -n $rnx_file ]]; then
+		# Selection of date from filename
+		date=$(basename "$rnx_file")
         
-        sudo mkdir -p /etc/systemd/system/
-        
-        echo "${template//\$\{path\}/$path}" | \
-        sed "s/\$receiver/$receiver/g" | \
-        sed "s/\$date/$date/g" | \
-        sed "s/\$user/$user/g" | sudo tee "/etc/systemd/system/$receiver.service" > /dev/null
-        
-        sudo systemctl daemon-reload
-        sudo systemctl enable "$receiver.service"
-    fi
+		sudo mkdir -p /etc/systemd/system/
+
+		# Inserting         
+		echo "${template//\$\{path\}/$path}" | \
+		sed "s/\$receiver/$station/g" | \
+		sed "s/\$date/$date/g" | \
+		sed "s/\$user/$user/g" | sudo tee "/etc/systemd/system/$station.service" > /dev/null
+
+		sudo systemctl daemon-reload
+		sudo systemctl enable "$station.service"
+	fi
 done
