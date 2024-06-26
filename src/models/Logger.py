5ff326d7db6b9dcf12b53381ac210828f1cbe978
DemidAntipin <antipindemid@inbox.ru>
2024-06-26 05:14:07 +0000

    Added Logger and added function log in publisher and subscriber

--- src/models/Logger.py
+++ src/models/Logger.py
@@ -1,38 +1,43 @@
 import logging
 import sys
+import os
 import socket
-from logging.handlers import TimedRotatingFileHandler
-
+from logging.handlers import RotatingFileHandler
 
 class Logger:
-    FORMATTER_STRING = f"%(asctime)s — {socket.gethostbyname(socket.gethostname())} — %(name)s" \
-                       f" — %(levelname)s — %(message)s"
-    FORMATTER = logging.Formatter(FORMATTER_STRING)
-    LOG_FILE = "logs.log"
+	FORMATTER_STRING = f"%(asctime)s %(name)s" \
+	f" %(levelname)s %(message)s"
+	FORMATTER = logging.Formatter(FORMATTER_STRING)
+	LOG_FILE = os.path.join(os.path.dirname(__file__), '..', '..', 'logs.log')
+	MAX_BYTES = 10 * 1024 * 1024 # 10 МБ
+	BACKUP_COUNT = 5 # Сохранять 5 резервных копий
 
-    def __init__(self, name: str, console_out: bool = False):
-        logger = logging.getLogger(name)
-        logger.setLevel(logging.DEBUG)
+	# Создаем глобальный логгер
+	logger = logging.getLogger(__name__)
+	logger.setLevel(logging.DEBUG)
 
-        if console_out:
-            stdout_handler = logging.StreamHandler(sys.stdout)
-            stdout_handler.setFormatter(Logger.FORMATTER)
-            logger.addHandler(stdout_handler)
+	# Настраиваем обработчики (только один раз!)
+	file_handler = RotatingFileHandler(LOG_FILE, maxBytes=MAX_BYTES, backupCount=BACKUP_COUNT, mode='a')
+	file_handler.setFormatter(FORMATTER)
+	logger.addHandler(file_handler)
 
-        file_handler = TimedRotatingFileHandler(Logger.LOG_FILE, when='midnight')
-        file_handler.setFormatter(Logger.FORMATTER)        
-        logger.addHandler(file_handler)
+	def __init__(self, name: str, console_out: bool = False):
+		# Устанавливаем имя логгера, если необходимо
+		self.logger.name = name
 
-        self.logger = logger
+		if console_out:
+			stdout_handler = logging.StreamHandler(sys.stdout)
+			stdout_handler.setFormatter(Logger.FORMATTER)
+			self.logger.addHandler(stdout_handler)
 
-    def add_debug(self, text: str):
-        self.logger.debug(text)
+	def add_debug(self, text: str):
+		self.logger.debug(text)
 
-    def add_info(self, text: str):
-        self.logger.info(text)
+	def add_info(self, text: str):
+		self.logger.info(text)
 
-    def add_warning(self, text: str):
-        self.logger.warning(text)
+	def add_warning(self, text: str):
+		self.logger.warning(text)
 
-    def add_error(self, text: str):
-        self.logger.error(text)
+	def add_error(self, text: str):
+		self.logger.error(text)
